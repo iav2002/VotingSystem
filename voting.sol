2faf99f22c441c7a0a7df93af2b0f3c459911b14
bergandara <gandaraber@gmail.com>
2023-11-23 16:57:01 +0000

    stopVoting and addVote functions created

--- voting.sol
+++ voting.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
 
-contract voting{
+contract voting {
     //Structure of Voting
 
-    struct Vote{
+    struct Vote {
         address receiver;
         uint256 timestamp; //
 
@@ -18,15 +18,31 @@ contract voting{
     event StartVoting(address startedBy);
     event StopVoting(address stoppedBy);
 
-    constructor() public{
+    constructor() public {
         isVoting = false;
     }
 
-    function startVoting() external returns(bool){
+    function startVoting() external returns (bool) {
         isVoting = true;
         emit StartVoting(ms.sender);
         return true;
-
     }
 
+    function stopVoting() external returns (bool) {
+        isVoting = false;
+        emit stopVoting(msg.sender);
+        return true;
+    }
+
+    function addVote(address receiver) external returns (bool) {
+        votes[msg.sender].receiver = receiver;
+        votes[msg.sender].timestamp = now;
+
+        emit AddVote(
+            msg.sender,
+            votes[msg.sender].receiver,
+            votes[msg.sender].timestamp
+        );
+        return true;
+    }
 }
