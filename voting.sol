38261f936771db893cb16454287428ce59ff76a7
bergandara <gandaraber@gmail.com>
2023-11-30 16:11:56 +0000

    getBalance method created, returns balance of the contract to be used in addVoteTo()

--- voting.sol
+++ voting.sol
@@ -53,4 +53,9 @@ contract voting {
     function getVote() public payable {
         balancedReceived += msg.value; //Update the balance received
     }
+
+    //Retains the contract's balance
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
 }
